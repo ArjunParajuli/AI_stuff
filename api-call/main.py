485272d58ceb3e91f59c6734729781e02b237af6
Arjun-Parajuli <chhetriarjun55555@gmail.com>
2026-02-13 11:04:41 +0530

    Zero-shot prompting

--- api-call/main.py
+++ api-call/main.py
@@ -10,14 +10,22 @@ client = OpenAI(
     api_key=os.getenv("OPENROUTER_API_KEY") 
 )
 
+system_prompt = """
+You are a coding assistant named Alexa.
+You must answer only coding-related questions.
+If the user asks anything unrelated to coding, reply only with:
+"Sorry, I can only answer coding-related questions."
+"""
+
 response = client.chat.completions.create(
     model="meta-llama/llama-3.3-70b-instruct:free",  
     messages=[
+        {"role": "system", "content": system_prompt},
         {
             "role": "user",
-            "content": "Hey there, I am Arzun and nice to meet you."
+            "content": "Hey there, I am Arzun and nice to meet you. Whats your name?"
         }
     ]
 )
 
-print(response.choices[0].message.content)
+print(response.choices[0].message)
